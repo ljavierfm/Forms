8d9ada6879503d78f0d56a725a7cbd5ea79676c0
Javier Fernandez <ljavierfm@gmail.com>
2018-05-03 12:08:50 +0200

    Recibir valores desde ngForm al enviar formulario

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  usuario: Object = {
+    nombre: 'Javier',
+    apellido: 'Fernández',
+    email: 'bitoman@gmail.com'
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  guardar(forma: NgForm) {
+    console.log("Formulario posteado");
+    console.log("ngForm", forma);
+    console.log("Valor: ", forma.value);
+    console.log("Usuario: ",this.usuario);
+
+    let chain='';
+    forma.valid?chain='valid':chain="invalid";
+
   }
 
 }
