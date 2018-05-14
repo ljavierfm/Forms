a962d1c14dbffdb6cdeda7051ea145887805c9d2
Javier Fernandez <ljavierfm@gmail.com>
2018-05-14 10:29:07 +0200

    Validacion usando FormGroup desde ts

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-data',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
+
+  usuario: any = {
+    nombreCompleto: {
+      nombre: 'Javier',
+      apellido: 'Fernández'
+    },
+    correo: 'bitoman@gmail.com'
+  }
+
+  constructor() {
+
+    console.log(this.usuario);
+
+
+    this.forma = new FormGroup(
+      {
+        'nombreCompleto': new FormGroup({
+          'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
+          'apellido': new FormControl('', Validators.required)
+        }),
+        'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
+      }
+    )
+
+    this.forma.setValue(this.usuario);
+  }
+
+  guardarCambios() {
+    console.log(this.forma);
+    this.forma.reset({
+      nombreCompleto: {
+        nombre: '',
+        apellido: ''
+      },
+      correo: ''
+    }
+    );
+  }
 
   ngOnInit() {
+    console.log(this.forma);
   }
 
 }
